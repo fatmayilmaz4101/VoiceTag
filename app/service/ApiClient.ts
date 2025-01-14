bef86eec5f8a8fb627ad2525c3cfafccf045c58b
Fatma Yılmaz <79848362+fatmayilmaz4101@users.noreply.github.com>
2025-01-14 22:03:42 +0300

    update

--- app/service/ApiClient.ts
+++ app/service/ApiClient.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "http://192.168.1.117:3030/api";
+const API_BASE_URL = "http://192.168.189.231:3030/api";
 
 const ApiClient = axios.create({
   baseURL: API_BASE_URL,
